1ed61be0333cbf51badf9295c2eddbc15495576f
wangzhibo <wangzhibo@neofaith.net>
2024-11-01 11:09:44 +0800

    feat: 截图分享

--- src/pages/demo/Chart/index.tsx
+++ src/pages/demo/Chart/index.tsx
@@ -4,6 +4,7 @@ import { Chart } from '@/chart-constructor/view';
 import { Button, Flex } from 'antd';
 import { VerticalAlignBottomOutlined } from '@ant-design/icons';
 import { Header } from '../components/Headerlayout';
+import html2canvas from 'html2canvas';
 
 import './index.less';
 
@@ -11,13 +12,30 @@ type SettingProps = {
   options: EChartsOption;
 };
 export const ChartView: React.FC<SettingProps> = (props) => {
+
+  const chartRef = React.useRef<HTMLDivElement>(null);
+  const handleScreenshot = async () => {
+    if (chartRef.current) {
+      try {
+        const canvas = await html2canvas(chartRef.current);
+        const imgData = canvas.toDataURL('image/png');
+        const link = document.createElement('a');
+        link.href = imgData;
+        link.download = 'chart-screenshot.png';
+        link.click();
+      } catch (error) {
+        console.error('截图失败:', error);
+      }
+    }
+  };
+
   const HeaderBtns = () => {
     return (
       <Flex gap={8} style={{ paddingLeft: 24 }}>
         <Button type='primary' icon={<VerticalAlignBottomOutlined />}>
           下载示列
         </Button>
-        <Button>截图分享</Button>
+        <Button onClick={handleScreenshot}>截图分享</Button>
       </Flex>
     );
   };
@@ -27,7 +45,7 @@ export const ChartView: React.FC<SettingProps> = (props) => {
       <Header>
         <HeaderBtns />
       </Header>
-      <div className='chart_wrapper'>
+      <div className='chart_wrapper' ref={chartRef}>
         <Chart options={props.options} />
       </div>
     </div>
