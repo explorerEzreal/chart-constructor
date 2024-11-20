f2f71b6c418755a8b397f1931c4fb32ea4731ebf
chenhong <1690599634@qq.com>
2024-11-20 15:59:47 +0800

    Suspense组件懒加载的样式以及文字设置；

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,7 +2,8 @@ import { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { routes } from './route';
 import PageLayout from './Layout';
-
+import {  Spin } from 'antd';
+import { textAlign } from 'html2canvas/dist/types/css/property-descriptors/text-align';
 
 export function Routers() {
   const modules = import.meta.glob('/src/pages/**');
@@ -24,7 +25,11 @@ export function Routers() {
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    // <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div style={{width:'100vw',height:'100vh',backgroundColor:'#fff',fontSize:30,color:'black',display:'flex',justifyContent:'center',alignItems:'center'}}>
+      <Spin tip="Loading" size="large"></Spin>
+      <span style={{padding:'0 20px'}}>加载中...</span>
+      </div>}>
       <Routes>
         <Route key='redirect' path='/' element={<Navigate to='/home' />} />
         {routes.map((item) => {
