f5414aa90ae5e45d3b8b496bb0262aaf153dd5b3
tanxianduo <tanxianduo@skieer.com>
2024-11-20 16:09:36 +0800

    feat: 新增loading组件。

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,8 +2,29 @@ import { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { routes } from './route';
 import PageLayout from './Layout';
-import {  Spin } from 'antd';
-import { textAlign } from 'html2canvas/dist/types/css/property-descriptors/text-align';
+import { Spin } from 'antd';
+
+
+// TODO: CH 抽离样式
+function PageLoading() {
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: '#fff',
+        fontSize: 30,
+        color: 'black',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Spin tip='Loading' size='large'></Spin>
+      <span style={{ padding: '0 20px' }}>加载中...</span>
+    </div>
+  );
+}
 
 export function Routers() {
   const modules = import.meta.glob('/src/pages/**');
@@ -25,11 +46,7 @@ export function Routers() {
   };
 
   return (
-    // <Suspense fallback={<div>Loading...</div>}>
-    <Suspense fallback={<div style={{width:'100vw',height:'100vh',backgroundColor:'#fff',fontSize:30,color:'black',display:'flex',justifyContent:'center',alignItems:'center'}}>
-      <Spin tip="Loading" size="large"></Spin>
-      <span style={{padding:'0 20px'}}>加载中...</span>
-      </div>}>
+    <Suspense fallback={<PageLoading />}>
       <Routes>
         <Route key='redirect' path='/' element={<Navigate to='/home' />} />
         {routes.map((item) => {
